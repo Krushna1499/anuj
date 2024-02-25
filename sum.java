fef6ac82889bfdf0be896b265e8d944ecf1ec0a5
Krishna <lkrushana@gmail.com>
2024-02-25 11:40:54 +0530

    second commit

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ public class sum
     public static void main(String[] args) {
         int a=10;
         int b=20; 
-        System.out.println(a+b);
+        System.out.println("The sum is: "+a+b);
     }
 
     
